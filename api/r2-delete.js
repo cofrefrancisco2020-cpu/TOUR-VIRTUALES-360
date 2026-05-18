21f6d61d30e11f41a4099f31e9ea24d5bf3b1869
cofrefrancisco2020-cpu <cofrefrancisco2020@gmail.com>
2026-05-18 13:10:50 -0400

    Add files via upload

--- api/r2-delete.js
+++ api/r2-delete.js
@@ -1,7 +1,7 @@
-const { handleR2Presign } = require("../r2-presign.cjs");
+const { handleR2Delete } = require("../r2-presign.cjs");
 
 module.exports = function handler(req, res) {
-  return handleR2Presign(req, {
+  return handleR2Delete(req, {
     writeHead(status, headers) {
       res.statusCode = status;
       Object.entries(headers || {}).forEach(([key, value]) => res.setHeader(key, value));
